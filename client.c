fecc152e726ad9ec7986d730568553eb1515732f
ltouret <jiloh55@gmail.com>
2025-03-10 23:56:04 +0100

    Added skeleton of client and server

---
+++ client.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+// #include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#define ICMP_ECHO 8
+#define ICMP_ECHO_REPLY 0
+
+struct icmp
+{
+    // header 20 bytes
+    uint8_t icmp_type;
+    uint8_t icmp_code;
+    uint16_t icmp_cksum; // padding
+    uint16_t icmp_id;
+    uint16_t icmp_seq;
+    uint32_t icmp_otime;
+    uint32_t icmp_rtime; // padding
+    uint32_t icmp_ttime;
+
+    // padding 4 bytes
+    uint32_t padding;
+
+    // data 40 bytes
+    // uint8_t data[40];
+    uint8_t username[10];
+    uint8_t roomname[10];
+    //! try with more len for message
+    uint8_t message[20];
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <destination_ip>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int sockfd;
+    struct sockaddr_in dest_addr;
+    struct icmp icmp_packet =
+    {
+        .username = "user",
+        .roomname = "room",
+        .message = "message"
+    };
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+    if (sockfd < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    dest_addr.sin_family = AF_INET;
+    if (inet_pton(AF_INET, argv[1], &dest_addr.sin_addr) <= 0)
+    {
+        perror("inet_pton");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    icmp_packet.icmp_type = ICMP_ECHO;
+    // icmp_packet.icmp_id = getpid();
+    // icmp_packet.icmp_seq = 1;
+    // memset(&icmp_packet.data, 'A', sizeof(icmp_packet.data));
+
+    if (sendto(sockfd, &icmp_packet, sizeof(icmp_packet), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0)
+    {
+        perror("sendto");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    close(sockfd);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+// #include <netinet/ip_icmp.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+// # define ICMP_
+#define ICMP_ECHO 8
+
+struct icmp
+{
+    // header 20 bytes
+    uint8_t icmp_type;
+    uint8_t icmp_code;
+    uint16_t icmp_cksum; // padding
+    uint16_t icmp_id;
+    uint16_t icmp_seq;
+    uint32_t icmp_otime;
+    uint32_t icmp_rtime; // padding
+    uint32_t icmp_ttime;
+
+    // padding 4 bytes
+    uint32_t padding;
+
+    // data 40 bytes
+    // uint8_t data[40];
+    uint8_t username[10];
+    uint8_t roomname[10];
+    //! try with more len for message
+    uint8_t message[20];
+};
+
+//! maybe max rooms to not get ddos out of memory by some random?
+struct room
+{
+    char roomname[11];
+    char users[100][11];
+    struct room *next;
+};
+
+
+//! create room if it doesnt exist
+void create_room(struct room *room, const char *roomname)
+{
+    if (room == NULL)
+    {
+        //malloc
+    }
+    // if roomname exists done
+    struct room *current = room;
+    while (current)
+    {
+        if (current->roomname == roomname)
+        {
+            break;
+        }
+        current = current->next;
+    }
+    // else create
+    // malloc and add to linked list
+}
+
+int main()
+{
+    int sockfd;
+    struct room *room = NULL;
+    unsigned char buffer[1000] = {0}; // Buffer to hold the packet
+
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("ICMP socket created successfully.\n");
+
+    while (1)
+    {
+        ssize_t bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
+        if (bytes_received < 0)
+        {
+            perror("recvfrom");
+            continue; // Or handle the error as needed
+        }
+
+        // Parse the IP header
+        struct iphdr *ip_header = (struct iphdr *)buffer;
+
+        // Verify that the packet is ICMP
+        if (ip_header->protocol == IPPROTO_ICMP)
+        {
+            // Parse the ICMP header
+            struct icmp *icmp_header = (struct icmp *)(buffer + ip_header->ihl * 4); // Skip IP header
+
+            // You can further process the ICMP data here
+            if (icmp_header->icmp_type == ICMP_ECHO)
+            {
+                // printf("Received %zd bytes.\n", bytes_received);
+
+                // printf("IP Header:\n");
+                // printf("  Version: %u\n", ip_header->version);
+                // printf("  Header Length: %u\n", ip_header->ihl * 4);
+                // printf("  Source IP: %s\n", inet_ntoa(*(struct in_addr *)&ip_header->saddr));
+                // printf("  Destination IP: %s\n", inet_ntoa(*(struct in_addr *)&ip_header->daddr));
+                // printf("  Protocol: %u\n", ip_header->protocol);
+
+                printf("ICMP Header:\n");
+                // printf("  Type: %u\n", icmp_header->icmp_type);
+                // printf("  Code: %u\n", icmp_header->icmp_code);
+                printf("  %s\n", icmp_header->username);
+                printf("  %s\n", icmp_header->roomname);
+                printf("  %s\n", icmp_header->message);
+
+                //! if room doesnt exist create
+            }
+        }
+        else
+        {
+            printf("Packet is not ICMP\n");
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
